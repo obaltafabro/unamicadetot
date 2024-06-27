872af96d1991a8de160d73cd8bece5d31ac00b6e
obaltafabro <68032508+obaltafabro@users.noreply.github.com>
2024-06-27 19:47:02 +0200

    Commit fins el segon

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("unamicadetot.urls")),
+    path("unamicadetot/", include("unamicadetot.urls")),
     path('admin/', admin.site.urls),
 ]
--- unamicadetot/views.py
+++ unamicadetot/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hola, ets a l'index de l'aplicació")
